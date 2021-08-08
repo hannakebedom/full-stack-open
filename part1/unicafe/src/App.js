a375ba92ab0a0c6c4b09914659c9e91bf60aef16
Hanna Kebedom <hanna.kebedom@shopify.com>
2021-08-08 18:12:55 -0400

    1.9 unicafe step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,6 +15,14 @@ const Display = ({name, count}) => {
 }
 
 const Statistics = ({good, neutral, bad, all, average, positive}) => {
+  if (all == 0) {
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </>
+    )
+  }
   return (
     <>
     <h1>statistics</h1>
