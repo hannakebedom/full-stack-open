9da23a163abcd183abae2d190f7d6d9239148db2
Hanna Kebedom <hanna.kebedom@shopify.com>
2021-08-21 11:49:48 -0400

    2.8 & 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
+  const [ newSearch, setNewSearch] = useState('')
 
   // Add a person to the phonebook
-  const addName =  (event) => {
+  const addPerson =  (event) => {
     event.preventDefault()
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (persons.some(person => person.name === newName)) {
       // Alert user that person is already in the phonebook
@@ -22,6 +28,7 @@ const App = () => {
     
     //Reset input box
     setNewName('')
+    setNewNumber('')
   }
 
   // Event handler that synchronizes the changes made to the input with the component's state
@@ -29,19 +36,35 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  const peopleToShow = !(newSearch === '') ? persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())) : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <div>
+          filter shown with <input value={newSearch} onChange={handleSearchChange}/>
+      </div>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p>{person.name}</p>)}
+      {peopleToShow.map(person => <p>{person.name}</p>)}
     </div>
   )
 }
